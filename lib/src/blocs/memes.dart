002f447c23e81474cae3104179862797dbd171aa
KarimElghamry <Karimelghamry@gmail.com>
2019-07-11 16:55:29 +0200

    added provider to materialapp

--- lib/src/blocs/memes.dart
+++ lib/src/blocs/memes.dart
@@ -1,13 +1,22 @@
 import 'package:memec/src/models/meme.dart';
 import 'package:rxdart/subjects.dart';
+import 'package:http/http.dart' as http;
 
 class MemesBloc {
+  String _apiUrl;
   BehaviorSubject<Meme> _currentMeme$;
 
   BehaviorSubject<Meme> get currentMeme$ => _currentMeme$;
 
   MemesBloc() {
     _currentMeme$ = BehaviorSubject<Meme>();
+    _apiUrl = "https://meme-api.herokuapp.com/gimme";
+    fetchNextMeme();
+  }
+
+  Future<void> fetchNextMeme() async {
+    final _response = await http.get(_apiUrl);
+    print(_response.body);
   }
 
   void dispose() {
--- lib/src/root.dart
+++ lib/src/root.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:memec/src/blocs/global.dart';
 import 'package:memec/src/ui/home/home_screen.dart';
+import 'package:provider/provider.dart';
 
 class Memec extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return Provider(
+      builder: (BuildContext context) => GlobalBloc(),
+      dispose: (BuildContext context, GlobalBloc value) => value.dispose(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
   }
 }
